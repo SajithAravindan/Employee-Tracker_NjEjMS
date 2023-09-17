89cac00fc2cc8aea8d418d052f7ea6f5bd4bda8e
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-09-16 21:25:29 -0400

    Run SQL files and created the Database, Tables with Sample Data

--- db/schema.sql
+++ db/schema.sql
@@ -12,23 +12,23 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
-  department_id INT NOT NULL
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
-  ON DELETE RESTRICT ON UPDATE CASCADE
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
+  role_id INT,
   manager_id INT NULL,
   FOREIGN KEY (role_id)
         REFERENCES role(id)
-        ON DELETE RESTRICT ON UPDATE CASCADE,
+        ON DELETE SET NULL,
   FOREIGN KEY (manager_id)
         REFERENCES employee(id)
         ON DELETE SET NULL 
-        ON UPDATE CASCADE
+        
 );
