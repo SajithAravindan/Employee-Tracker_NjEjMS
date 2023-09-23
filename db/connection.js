a143685d60a1e606cd82e13c57233e41542c6a8d
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-09-23 00:35:09 -0400

    Worked on Readme file and connection file

--- db/connection.js
+++ db/connection.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 const dbConnection = mysql.createConnection(
   {
     host: "localhost",
-    user: "root",
-    password: "mysql@0411",
-    database: "employees_db",
+    user: "<user>",
+    password: "<your password>",
+    database: "<database name.",
   },
   console.log("Connected to the Employee Tracking database.")
 );
