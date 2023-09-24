0b1f81c516dcdd5df48b2a84126c2f54f1322f39
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-09-24 01:10:01 -0400

    Added Comments for code base

--- db/connection.js
+++ db/connection.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 const dbConnection = mysql.createConnection(
   {
     host: "localhost",
-    user: "<user>",
-    password: "<your password>",
-    database: "<database name>",
+    user: "root",
+    password: "mysql@0411",
+    database: "employees_db",
   },
   console.log("Connected to the Employee Tracking database.")
 );
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 //Global Variables
-const mysql = require('mysql2');
-const inquier = require('inquirer');
 const startQuestions = require('./lib/main_menu');
 
 function init(){
-    startQuestions();//initialize the application
+    startQuestions();//Start the application
 } 
 
-init(); 
+init(); //initialize the application
